c69b27c18235d4bf825ff8f45051c6e11546fa12
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-09-10 10:29:16 +0530

    Update .gitignore

--- controllers/getAllSliders.js
+++
@@ -1,7 +0,0 @@
-
-const Slider = require("../models/Slider");
-
-exports.getAllSliders = async (req, res) => {
-  const response = await Slider.find().populate("linkedMovie")
-  return res.status(200).json({ Slider: response });
-};
