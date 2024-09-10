7a1e188af3292d025ef33b994cc33621d56d4bc5
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-09-10 10:29:16 +0530

    Update .gitignore

--- models/Slider.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const sliderSchema = new mongoose.Schema({
-  schemaName: {type:String},
-  type: String,
-  linkedMovie: {type: mongoose.Schema.Types.ObjectId, ref: "Movies"}
-});
-
-const Slider=mongoose.model("Slider",sliderSchema)
-module.exports=Slider
