a5d4c13bb7ba576d2422af7522d27f374964f66f
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-10-25 18:06:00 +0530

    added check in points allotement

--- models/checkInPoints.js
+++ models/checkInPoints.js
@@ -1,6 +1,8 @@
-const mongoose=require("mongoose")
-const checkInpointsSchema=new mongoose.Schema({
-    Day:Number, title:String, allocatedPoints:Number
-})
-const checkInpoints=mongoose.model("checkInPoints",checkInpointsSchema)
-module.exports=checkInpoints
+const mongoose = require("mongoose");
+const checkInpointsSchema = new mongoose.Schema({
+  Day: Number,
+  title: String,
+  allocatedPoints: Number,
+});
+const checkInpoints = mongoose.model("checkInPoints", checkInpointsSchema);
+module.exports = checkInpoints;
