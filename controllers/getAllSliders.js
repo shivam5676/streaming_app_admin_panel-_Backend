5d933eeb178b6dec43a7c32fff5c31c46a8717b9
Shivam Singh <ssultanpura9@gmail.com>
2025-02-24 18:06:03 +0530

    added pagination in and serach query in users

--- controllers/getAllSliders.js
+++ controllers/getAllSliders.js
@@ -1,13 +1,38 @@
 const Slider = require("../models/Slider");
 
 exports.getAllSliders = async (req, res) => {
+  const { start, limit, searched } = req.query;
+  // console.log(start, limit,searched);
+
+  let filter = {};
   try {
-    const response = await Slider.find().populate("linkedMovie");
-    if(!response){
+    if (searched && searched.trim() !== "") {
+      filter = {
+        $or: [
+          { schemaName: { $regex: searched, $options: "i" } },
+          { type: { $regex: searched, $options: "i" } },
+          { promotionalImageUrl: { $regex: searched, $options: "i" } },
+          { RedirectionLink: { $regex: searched, $options: "i" } },
+        ],
+      };
+    }
+    const totalSliders = await Slider.countDocuments(filter);
+    const response = await Slider.find(filter)
+      .populate("linkedMovie")
+      .skip(limit * start)
+      .limit(limit);
+    if (!response) {
       return res.status(200).json({ Slider: [] });
     }
-    return res.status(200).json({ Slider: response });
+    return res.status(200).json({
+      Slider: response,
+      start,
+      limit,
+      totalData: totalSliders,
+      totalPages: Math.ceil(totalSliders / limit),
+    });
   } catch (error) {
-    return res.status(400).json({ err:error });
+    console.log(error);
+    return res.status(400).json({ msg: "something went wrong", err: error });
   }
 };
